1538d6dfe843baa945c13409f11d7c710d7a3147
Deven Lahoti <deven.lahoti@gmail.com>
2018-10-09 20:05:30 -0400

    fix autosub script for nix

--- dotfiles/mpv/scripts/autosub.lua
+++ dotfiles/mpv/scripts/autosub.lua
@@ -4,14 +4,15 @@
 -- keyname script_binding auto_load_subs
 local utils = require 'mp.utils'
 function load_sub_fn()
-    subl = "/usr/bin/subliminal" -- use 'which subliminal' to find the path
+    nixsh = "/run/current-system/sw/bin/nix-shell"
     mp.msg.info("Searching subtitle")
     mp.osd_message("Searching subtitle")
+    subl = table.concat({"subliminal", "download", "-s", "-l", "en", "'" .. mp.get_property("path") .. "'"}, " ")
     t = {}
-    t.args = {subl, "download", "-s", "-l", "en", mp.get_property("path")}
+    t.args = {nixsh, "-p", "python27Packages.subliminal", "--run", subl}
     res = utils.subprocess(t)
     if res.status == 0 then
-        mp.commandv("rescan_external_files", "reselect") 
+        mp.commandv("rescan_external_files", "reselect")
         mp.msg.info("Subtitle download succeeded")
         mp.osd_message("Subtitle download succeeded")
     else
